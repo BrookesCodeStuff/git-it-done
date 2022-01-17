b185ac36385f1ab1a08f9214947835f27e1bb72a
Brooke <brookepaglia@gmail.com>
2022-01-17 17:42:29 -0500

    add page redirects for invalid usernames

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,14 +1,39 @@
 var issueContainerEl = document.querySelector('#issues-container');
+var limitWarningEl = document.querySelector('#limit-warning');
+var repoNameEl = document.querySelector('#repo-name');
+
+var getRepoName = function () {
+  // Grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split('=')[1];
+
+  if (repoName) {
+    // Display the repo name on the page
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    // If no repo was given, redirect to the homepage
+    document.location.replace('./index.html');
+  }
+};
 
 var getRepoIssues = function (repo) {
   var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc';
+  // Make a request to the apiurl
   fetch(apiUrl).then(function (response) {
+    // Request was successful
     if (response.ok) {
       response.json().then(function (data) {
         displayIssues(data);
+
+        // Check if api has paginated issues
+        if (response.headers.get('Link')) {
+          displayWarning(repo);
+        }
       });
     } else {
-      alert('Thhis was a problem with your request!');
+      // Request wasn't successful, go back to home
+      document.location.replace('./index.html');
     }
   });
 };
@@ -48,4 +73,17 @@ var displayIssues = function (issues) {
   }
 };
 
-getRepoIssues('brookescodestuff/password_generator');
+var displayWarning = function (repo) {
+  // Add text to warning container
+  limitWarningEl.textContent = 'To see more than 30 issues, visit ';
+
+  var linkEl = document.createElement('a');
+  linkEl.textContent = 'github.com';
+  linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
+  linkEl.setAttribute('target', '_blank');
+
+  // Append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
